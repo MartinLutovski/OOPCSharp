ed39a2d1a5d74209f959cc2ae45280db13607eab
Your Name <you@example.com>
2025-03-06 18:33:12 +0100

    Class04 exercise

---
+++ Class04/Qinshift.Exercise/Qinshift.Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Qinshift.Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter another number: ");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a mode (+ or -): ");
+                int mode = Convert.ToChar(Console.ReadLine());
+                if (mode == '+')
+                {
+                    Console.WriteLine("Sum: " + Sum(numOne, numTwo));
+                    break;
+                }
+                else if (mode == '-')
+                {
+                    Console.WriteLine("Sub: " + Sub(numOne, numTwo));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
+
+            }
+
+
+        }
+        
+        public static int Sum(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+        public static int Sub(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+    }
+}
